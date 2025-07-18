a5dd92204528f4be615deabea227d8ac86168f0d
suhanipandey04 <dheeverse@gmail.com>
2025-07-18 12:19:54 +0530

    conditionals loop

---
+++ day3.cpp
@@ -0,0 +1,181 @@
+/*conditionals loops*/
+#include<iostream>
+using  namespace std;
+int main()
+{
+   /*int n;
+    cin>>n;/*cin means taking input of n
+    cout<<"value of n is:"<< n << endl;
+    if(n>0)
+    {
+        cout<<"n is positive"<< n <<endl;
+    }
+    else
+    {
+       cout<<"n is negative"<< n <<endl;
+    }*/
+
+    /*int a;
+    cin>>a;
+    cout<<"value of a is:"<< a <<endl;
+    int b;
+    cin>>b;
+    cout<<"value of b is:"<< b <<endl;
+    //we can also take input of both a and b together by cin>>a>>b like this
+    if(a>b){
+        cout<<"a is greater than b"<<endl;
+    }
+    else{
+        cout<<"b is greater than a"<<endl;
+    }*/
+
+    /*int a ;
+    cin>>a;
+    cout<<" the value of a is :"<< a << endl;
+    if(a>0){
+        cout<<"a is positive"<<endl;
+    }
+    else{
+        if(a<0){
+            cout<<"a is negative"<<endl;
+        }
+        else{
+            cout<<"a is 0"<<endl;
+        }
+    }*/
+    
+    /*int a ;
+    cin>>a;
+    cout<<"value of a :"<<a<<endl;
+    int b;
+    cin>>b;
+    cout<<"value of b:"<<b<<endl;
+    if(a>b){
+        cout<<"a is greater"<<endl;
+    }
+    else if(a<b){
+        cout<<"b is greater"<<endl;
+    }
+    else{
+        cout<<"both are equal"<<endl;
+    }*/
+
+   /* char ch;
+    cin>>ch;
+    cout<<"value of character:"<< ch <<endl;
+    if(isupper(ch))
+    {
+        cout<<"upper case alphabet"<<endl;
+    }
+    else if(islower(ch))
+    {
+        cout<<"is lower case alphabet"<<endl;
+    }
+    else if(isdigit(ch))
+    {
+        cout<<"is a digit"<<endl;
+    }
+    else
+    {
+        cout<<"invalid"<<endl;
+    }*/
+    
+    /*int n;
+    cin>>n;
+    cout<<"value of n is:"<<endl;
+    int i=1;
+    while(i<=n)
+    {
+        cout<<i<<endl;
+        i=i+1;
+    }*/
+    /*int n;
+    cin>> n;
+    int i =1;
+    int  sum = 0;
+    while(i<=n)
+    {
+        sum=sum+i;
+        i=i+1;
+    }
+    cout<<"value of sum is:"<<sum<<endl;*/
+    //sum of all even number
+    /*int n;
+    cin>>n;
+    int i=2;
+    int sum = 0;
+    while(i<=n){
+        i=i+2;
+        sum=sum+i;
+        
+        
+    }
+    cout<<"value of sum is:"<<sum<<endl;*/
+
+    //prime no
+   /* int n;
+    cin>>n;
+    int i =2;
+    while(i<n)
+    {
+        //divide hogya,not prime
+        if(n%i==0)
+        {
+            cout<<"not prime for "<< i <<endl;
+        }
+        else{
+            cout<<"prime for"<< i <<endl;
+        }
+        i=i+1;
+}*/    
+    
+    //pattern
+    /*int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j =1;
+        while(j<=n)
+        {
+            cout<<"*";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }*/
+
+    /*int n;
+    cin>>n;
+    int i =1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            cout<<i;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }*/
+   //farhenite to celsius conversion
+  /* float farhenite,celsius;
+   cin>>farhenite;
+   cout<<"temp in farhenite is:"<<farhenite<<endl;
+   celsius=(5.0/9.0)*(farhenite-32);
+   cout<<"temp in celsius:"<<celsius<<endl;*/
+
+        
+            
+}
+
+
+
+    
+    
+    
+
+
+
+
