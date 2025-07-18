08df410f4e5a9dc9de92e03cb8396f575b3c880c
suhanipandey04 <dheeverse@gmail.com>
2025-07-18 12:06:50 +0530

    add new file

---
+++ day2.cpp
@@ -0,0 +1,63 @@
+/*data types and variables*/
+#include<iostream>
+using namespace std;
+int main () 
+{
+/*    int a = 123;
+    cout<< a << endl;
+
+    char b ='v';
+    cout<< b << endl;
+
+    bool bl = true;
+    cout<< bl <<endl;
+
+    float f = 1.2;
+    cout<< f <<endl;
+
+    double d = 1.23;
+    cout<< d <<endl;
+
+    int size = sizeof(a);
+    cout<< "size of a is:" <<size <<endl;/*size is used to check the size of the datatypes*/
+
+   /* int a = 'a';
+    cout<< a << endl;/*print the ascii integer value of a that is 97*/
+     
+   /* char ch = 98;
+    cout<< ch <<endl;/*print the char value of 97 that is b*/
+
+   /* char ch1= 123456;
+    cout<< ch1 <<endl;/*chart has 4 bit but the last bit shows 64 so it will give us warning and print @*/
+
+    /*unsigned int a = 112;
+    cout<< a << endl;
+    
+    unsigned int b=-112;
+    cout<< b << endl;/*4294967184*/
+
+   /* int a = 2/5;
+    cout<<a<<endl;
+
+    float b = 2.5/5;
+    cout<<b<<endl;*/
+
+    int a =2;
+    int b=3;
+    bool first =(a==b);
+    cout<< first<< endl;
+
+    bool second =(a>b);
+    cout<< second<< endl;
+
+    bool third =(a<b);
+    cout<<third<< endl;
+
+    bool fourth =(a>=b);
+    cout<< fourth<< endl;
+
+
+
+
+   
+}
