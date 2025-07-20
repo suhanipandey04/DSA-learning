b84d4bfe2c68f52b744f21282c7303166569f74f
suhanipandey04 <dheeverse@gmail.com>
2025-07-20 11:51:43 +0530

    Selection sort

---
+++ day12.cpp
@@ -0,0 +1,41 @@
+//if element =n than round =n-1
+//time complexity=O(n^2)(worst case)
+//best case=array already sorted
+//use case=when size of array is small
+
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
